8e6b20159b29f9966ffbed1e3dd0863b8100ba48
Rockykhaleel <r3khaleelshariff@gmail.com>
2024-02-15 16:12:12 +0530

    fixed vite-config-js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +8,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "/index.html",
-        about: "/about.html",
       },
     },
   },
